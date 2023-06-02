82e2487500d1f017b867336b556a2d7bcae1207c
4ebuard <yartspetr@gmail.com>
2023-06-02 23:40:29 +0300

    solved

---
+++ flowers.js
@@ -0,0 +1,13 @@
+//var flower1
+//var flower2
+function lovefunc(flower1, flower2){
+    // moment of truth
+    let x = flower1 + flower2
+    if (x % 2 == 1)  {
+      return true
+  }
+  else {
+      return false
+  }
+}
+  
