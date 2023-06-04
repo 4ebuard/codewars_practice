96caaa0416463b94ebb6445a5698e14832114908
4ebuard <yartspetr@gmail.com>
2023-06-04 14:06:37 +0300

    even_or_odd solved from CW

---
+++ evenORodd check.js
@@ -0,0 +1,12 @@
+function evenOrOdd(number) {
+    if (number%2 === 0) {
+      return('Even')
+    } else {
+      return('Odd')
+    }
+  }
+
+
+  function even_or_odd(number) {
+    return number % 2 ? "Odd" : "Even"
+  }
